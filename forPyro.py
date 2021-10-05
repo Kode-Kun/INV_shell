ea21c529a1b17c684a1a5b4432a9f558b2cb776f
Kode-Kun <80693906+Kode-Kun@users.noreply.github.com>
2021-10-05 12:16:55 -0400

    Create forPyro.py
    To-do list for the INV shell

---
+++ forPyro.py
@@ -0,0 +1,13 @@
+'''
+
+Steps: 
+1. Create pandasMain.py
+2. Do all the panadas things
+3. Create function that creates new excel file 
+4. (optional) add other fancy stuff like writing in the excel file (again, all in its own function)
+5. import pandasMain.py in main.py
+6. Add all functions we'll be calling from the shell in the func_dict variable. (look at how the other function references are written in the dictionary)
+7. Test run
+8. Get some sleep
+
+'''
